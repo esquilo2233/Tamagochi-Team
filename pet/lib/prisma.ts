d6c30c27e5bf3baa7c46d2d1113485f8727b9e3c
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2026-03-12 19:50:45 +0000

    bug fix

--- pet/lib/prisma.ts
+++ pet/lib/prisma.ts
@@ -1,19 +1,14 @@
 // Load .env for local scripts and ensure DATABASE_URL is available
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
-const { Pool } = pkg;
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient({ adapter, log: ["error"] });
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        accelerateUrl: process.env.DATABASE_URL,
+        log: ["error"],
+    });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
